518a4d7bef1f24133adb35a2b09020f9dc768d5c
Henrik Storch <henrik@getsquareone.app>
2025-07-17 15:55:39 +1200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Tamatoa",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Tamatoa",
+            targets: ["Tamatoa"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Tamatoa"
+        ),
+        .testTarget(
+            name: "TamatoaTests",
+            dependencies: ["Tamatoa"]
+        ),
+    ]
+)
